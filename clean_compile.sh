a10e61a8d524899852b1dc6a10339cdb67095aa4
Thiago Alves <thiagao0860@gmail.com>
2022-07-24 06:02:39 -0300

    First template commit

---
+++ clean_compile.sh
@@ -0,0 +1,20 @@
+# this uses texlive (if you don't have texlive: sudo apt install texlive-full)
+cd src/
+count=0;
+declare -a folder_list;
+
+for dir in *; do
+    if [  -d "$dir" ]; then
+        folder_list[$count]="$dir";
+        ((count++));
+    fi
+done
+
+cd ../
+
+rm -r output
+for item in ${folder_list[*]}; do
+    mkdir -p "./output/$item"
+done
+
+source compile.sh
---
+++ compile.sh
@@ -0,0 +1,5 @@
+# this uses texlive (if you don't have texlive: sudo apt install texlive-full)
+
+cd src/
+pdflatex -output-directory '../output' main_tcc_ufu.tex
+cd ../
